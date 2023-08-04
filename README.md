6cc21e4fab6faf87a9f194bb1a317a5f66b1ad81
BCA Developer <developer@baycomp.com>
2023-08-04 16:29:29 -0400

    Filled in template with actual implementation

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# bca-npm-template
-This is designed to serve as a starter project for developing npm packages.
-
-## Getting Started
-Create a new repository by selecting "Use this template" from the Code page  then: 
-* renaming the workspace file to match the new repo name,
-* updating the package and repository name in package.json,
-* updating index.js with the names of all objects provided by your package, and
-* running npm install locally and check-in the generated package-lock and update package.json.
-Make sure to create a repository secret named NODE_AUTH_TOKEN with the value of a
-Personal Access Token with write (and hence read) access to the account packages.
+# emailer
+This is designed to enable sending emails from a BackEnd AWS application.  It assumes
+that one has:
+* access to an AWS account,
+* followed the AWS-documented procedure enabling to successfully send emails from
+a registered domain,
+* associated that domain in DNS with the deployed application address,
+* included the  folder named Config at the top-level of your application containing a
+file named config.js containing a const JSON named config,
+containing a key named "domain" whose value is the registered domain, and
+* included the .aws folder with credentials to the AWS account under the app profile.
--- index.js
+++ index.js
@@ -1,14 +1,65 @@
 /**
  * @file index.js
- * @fileOverview This module ....
+ * @fileOverview This module enables sending emails from a BackEnd AWS application.
  * @author BCA www.baycomp.com
  * @date Created 8/1/2023
  * @copyright Copyright &copy; Bay Computer Associates, Inc. 2021\n
  *      All rights reserved\n
  *      This file contains CONFIDENTIAL material
  */
-// import { ... } from "...";
 
-// export {
-//    ...
-// };
+/** @var {module} nodemailer Email library. */
+import nodemailer from 'nodemailer';
+
+/** @var {Object} config Required app configuration values.
+ * This assumes that the JSON is located in the app source
+ * as Config/config.js defined as const config = <JSON>.
+*/
+import { config } from '@bca-developer/utilities';
+
+/** @var {module} AWS SDK for communication with Amazon's DynamoDB. */
+import AWS from 'aws-sdk';
+
+/**
+ * A class for sending an email from the system to a specified user.
+ */
+class Emailer {
+
+  /**
+   * A class for safe read/write access to the billing table.
+   * @property  {Mail} transporter SES Mail client.
+   */
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      SES: new AWS.SES()
+    });
+  }
+
+  /**
+   * Send the message to the given user via email.
+   * @param {string} user The recipient's email address.
+   * @param {string} subject The email subject line.
+   * @param {string} message The email message.
+   * @param {ReadableStream} attachment Any attachment you'd like to send.
+   * @param {string} attachmentFileName The name of the attachment you'd like
+   * to send.
+   */
+  email(user, subject, message, attachment = undefined, attachmentFileName = undefined) {
+    this.transporter.sendMail({
+      from: `no-reply@${config.domain}`,
+      to: user,
+      subject: subject,
+      html: `<pre>${message}</pre>`,
+      attachments: (attachment) ? [{ content: attachment, filename: attachmentFileName }] : undefined,
+    },
+      (err, info) => {
+        if (err) {
+          console.error(`Error: ${err}; could not send message: ${message}`);
+        }
+      });
+  }
+}
+
+export {
+  Emailer
+};
